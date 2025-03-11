fd2234b86b2f7e78f401a2bf2d1deb4a07ff41b2
Aman soni <108587077+aam2002@users.noreply.github.com>
2025-03-11 15:07:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Getting the values form Local Storage
 document.addEventListener("DOMContentLoaded", () => {
   const task = JSON.parse(localStorage.getItem("task"));
   if (task) {
@@ -8,18 +9,18 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+//Saveing to Loacal Storage
 const saveToLocal = () => {
   localStorage.setItem("task", JSON.stringify(taskArray));
 };
-
 let taskArray = [];
 let temp = [];
 let actualArray;
 let dragStartIndex;
 let addTaskBtn = document.getElementById("addTaskBtn");
+let clearBtn = document.createElement("button");
 
-let clearBtn = document.getElementById("Clear_task");
-
+//Clear all Task Functionality
 clearBtn.addEventListener("click", (e) => {
   e.preventDefault();
   taskArray = [];
@@ -27,6 +28,7 @@ clearBtn.addEventListener("click", (e) => {
   saveToLocal();
 });
 
+//Add Task Button
 addTaskBtn.addEventListener("click", (e) => {
   e.preventDefault();
   if (!addTaskBtn.classList.contains("pause")) addTask();
@@ -48,7 +50,18 @@ const addTask = () => {
   }
 };
 
+//Update Task List
 const UpdateList = () => {
+  //Clear All button appearance
+  if (taskArray.length != 0) {
+    clearBtn.setAttribute("class", "btn");
+    clearBtn.setAttribute("id", "Clear_task");
+    clearBtn.setAttribute("novalidate", "novalidate");
+    clearBtn.innerText = "Clear All";
+    document.getElementById("input-taker").append(clearBtn);
+  } else {
+    clearBtn.remove();
+  }
   const TaskList = document.getElementById("draggable-list");
   TaskList.innerHTML = "";
   taskArray.forEach((task, index) => {
@@ -81,21 +94,26 @@ const UpdateList = () => {
     taskItem.addEventListener("change", () => checkerUpdate(index));
     TaskList.append(taskItem);
   });
+
+  //For Drag and Drop Feature
   addEventListeners();
 };
 
+//Task Completed or Not Checker
 const checkerUpdate = (index) => {
   taskArray[index].completed = !taskArray[index].completed;
   UpdateList();
   saveToLocal();
 };
 
+//Delete Functionality
 const Delete = (index) => {
   taskArray.splice(index, 1);
   UpdateList();
   saveToLocal();
 };
 
+//Edit Functionality
 const Edit = (index) => {
   if (!taskArray[index].completed) {
     temp = taskArray;
@@ -161,6 +179,7 @@ const Edit = (index) => {
   }
 };
 
+//Save Button
 const Save = (x) => {
   const task = document.getElementById("eTask").value;
   const discription = document.getElementById("eDiscription").value;
@@ -173,7 +192,7 @@ const Save = (x) => {
   saveToLocal();
 };
 
-//Edit Check Updater
+//On Edit Task is completed or Not Checker
 const EditcheckerUpdate = (index) => {
   temp[index].completed = !temp[index].completed;
 };
@@ -193,7 +212,9 @@ const addEventListeners = () => {
   });
 };
 
+//Drag Start
 function dragStart() {
+  //Getting the value of List from drag start is Triggered
   dragStartIndex = +this.closest("li").getAttribute("data-index");
 }
 function dragOver(e) {
@@ -212,6 +233,7 @@ function dragDrop() {
   swapItems(dragStartIndex, dragEndIndex);
 }
 
+//Swap functonility when Drag and Drop is used
 function swapItems(fromIndex, toIndex) {
   let t;
   t = taskArray[fromIndex];
